c36ac34f9a8e1d62de7841fae1c3eb71aa900071
IrvingC48 <c.ici.3015@hotmail.com>
2026-08-08 11:45:40 -0600

    Caso de uso: alcance local y global. Uso de funciones

---
+++ Modulo_3/funciones.py
@@ -0,0 +1,14 @@
+#1 El calculador de envios (Scope y globales)
+#Declarar nuestra variable Global (Accesible desde cualquier parte)
+
+TARIFA_BASE = 50 #Variable global
+
+def calcular_envio(peso):
+    costo = (peso * 10) + TARIFA_BASE
+    return costo
+
+precio_final = calcular_envio(10)
+print(f'El costo del envío es: {precio_final}')
+
+#Intento de Error
+# print(costo)
