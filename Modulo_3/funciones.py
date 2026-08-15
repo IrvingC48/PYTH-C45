a97943f07e7d5e0e1592878d6af6c0d9cccfdf4d
IrvingC48 <c.ici.3015@hotmail.com>
2026-08-15 11:33:43 -0600

    Caso 3: Conversor de Moneda Flexible

--- Modulo_3/funciones.py
+++ Modulo_3/funciones.py
@@ -4,10 +4,12 @@
 TARIFA_BASE = 50 #Variable global
 
 def calcular_envio(peso):
+    TARIFA_BASE = 51
     costo = (peso * 10) + TARIFA_BASE
     return costo
 
 precio_final = calcular_envio(10)
+print(TARIFA_BASE)
 print(f'El costo del envío es: {precio_final}')
 
 #Intento de Error
@@ -32,3 +34,19 @@ help(generar_titulo)
 #Llamada normal pasando argumentos
 generar_titulo("ANÁLISIS FINAL", "*")
 generar_titulo("ADVERTENCIA", "!")
+
+
+#3 Conversor de Moneda Flexible
+# Uso de parámetros obligatorios y opcionales
+
+#Tasa de cambio tiene un valor default de 17.14
+def convertir_a_pesos(cantidad, tasa_cambio=17.14):
+    total = cantidad * tasa_cambio
+    return round(total, 2)
+
+#Caso 1: Usando el valor por defecto (Dólares)
+print(f'100 USD son: {convertir_a_pesos(100)} MXN (Tasa Default)')
+
+#Caso 2: Sobreescribiendo el valor por defecto (Euros)
+pesos_euros = convertir_a_pesos(100, 19.80)
+print(f'100 Euros son: {pesos_euros} MXN (Tasa específica)')
