e81bd51b26254b0bc6cf928003f34f1dd0236b3d
IrvingC48 <c.ici.3015@hotmail.com>
2026-08-22 12:16:41 -0600

    App: Orquestador llamando a conversor

--- Modulo_3/app_conversor/app.py
+++ Modulo_3/app_conversor/app.py
@@ -1,1 +1,24 @@
 #Sistema que convierte importes entre Dólares, Euros y Pesos (o tu moneda local).
+import conversor
+import tasas #Solo para mostrar las opciones disponibles.
+
+def iniciar_programa():
+    print('---------CONVERSOR DE MONEDAS---------')
+    print(f'Monedas disponibles: {list(tasas.TASAS.keys())}')
+
+    try:
+        origen = input("Moneda de origen (ej. MXN):").upper()
+        destino = input("Moneda de destino (ej. USD):").upper()
+        monto = float(input("Cantidad a convertir:"))
+
+        resultado = conversor.realizar_conversion(monto,origen,destino)
+
+        if resultado is not None:
+            print(f'\nResultado: {monto} {origen} son {resultado} {destino}.')
+        else:
+            print(f'\nError: Por favor ingresa un número válido para el monto.')
+    except ValueError:
+        print('\nError: Por favor ingresa un número válido para el monto.')
+
+if __name__ == "__main__":
+    iniciar_programa()
